2f158504fbf3e8412b69a80370918579431fe0a2
Satendra Singh <satendra_singh@live.com>
2016-12-19 08:41:00 +0530

    merge sort partial

--- MergeSort/MergeSort.cs
+++ MergeSort/MergeSort.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MergeSort
 {
-    class MergeSort
+    public class MergeSort
     {
+        List<int> _list;
+        
+        public  MergeSort(IList<int> list)
+        {
+            Sort(list, 0, list.Count - 1);        
+        }
+
+        private void Sort(IList<int> list, int left , int right)
+        {
+            if (left < right)
+            {
+                int m = (left + right) / 2;
+
+                Sort(list, left, m);
+                Sort(list, m + 1, right);
+            }
+        }
+
+        private void Merge(IList<int> list, int left, int middle, int right)
+        {
+            var tempLeft = new List<int>();
+            var tempRight = new List<int>();
+
+            for (int i = left; i <= middle; i++)
+                tempLeft.Add(list[i]);
+
+            for (int i = middle + 1; i < right; i++)
+                tempRight.Add(list[i]);
+
+        }
     }
 }
